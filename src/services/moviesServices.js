6526bab68cc5d763a327e4e5dd791bccfb322cc8
Oliver McCormack <olivermccormack@Olivers-Mac-mini.local>
2025-06-12 20:35:52 +0100

    added .env variables

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -1,18 +1,19 @@
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // For poster images
 const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+const AUTH_KEY =process.env.REACT_APP_TMDB_AUTH_KEY;
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzhmZjY0Zjg3Yjc5YWFkNGNiYTNlMTkzODkyM2QzNiIsIm5iZiI6MTc0OTQ4NTQ5My42MDksInN1YiI6IjY4NDcwN2I1ZDM3NGVlZjM5ZjNmYzg4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4yepReQfYysoHZB1Hai_UvcMGSStf2ZNdc8m6_U1n0w'
+    Authorization: `Bearer ${AUTH_KEY}`
   }
 };
 
 // src/services/tmdbService.js
 import axios from "axios";
 
-const API_KEY = '878ff64f87b79aad4cba3e1938923d36'; //process.env.REACT_APP_TMDB_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
